136432be0cbfb99090ff176f4d67905ccc92e9f8
Geyslan G. Bem <geyslan@gmail.com>
2019-06-28 19:13:57 -0300

    cat: proper error handling
    Signed-off-by: Geyslan G. Bem <geyslan@gmail.com>

--- src/cat.c
+++ src/cat.c
@@ -4,16 +4,16 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "common.h"
+#include "error.h"
 
 #define BUFSIZE 8192
 char buf[BUFSIZE];
 
 static inline void closefd(int fd)
 {
-	if (close(fd) < 0) {
-		// treat error
-		exit(EXIT_FAILURE);
-	}
+	if (close(fd) < 0)
+		err_exit("closing fd %d", fd);
 }
 
 void cat(int in_fd)
@@ -22,21 +22,16 @@ void cat(int in_fd)
 	ssize_t nwritten;
 
 	while((nread = read(in_fd, buf, BUFSIZE)) > 0) {
-		if ((nwritten = write(STDOUT_FILENO, buf, nread)) < 0) {
-			// treat error
-			exit(EXIT_FAILURE);
-		}
-		if (nwritten < nread) {
-			// treat error
-			closefd(in_fd);
-			exit(EXIT_FAILURE);
-		}
+		if ((nwritten = write(STDOUT_FILENO, buf, nread)) < 0)
+			err_exit("writing to stdout");
+		if (nwritten == nread)
+			continue;
+		err_exit_n(EXIT_FAILURE,
+			   "written (%d) fewer bytes than read (%d)",
+			   nwritten, nread);
 	}
-
-	if (nread < 0) {
-		// treat error
-		exit(EXIT_FAILURE);
-	}
+	if (nread < 0)
+		err_exit("reading from fd %d", in_fd);
 }
 
 int is_same_rfile(int in_fd, char *fname)
@@ -49,18 +44,14 @@ int is_same_rfile(int in_fd, char *fname)
 	int out_isreg, in_isreg;
 	int flags;
 
-	if (fstat(in_fd, &sb) < 0) {
-		//treat error
-		exit(EXIT_FAILURE);
-	}
+	if (fstat(in_fd, &sb) < 0)
+		err_exit("fstat fd %d", in_fd);
 	in_dev = sb.st_dev;
 	in_ino = sb.st_ino;
 	in_isreg = S_ISREG(sb.st_mode);
 
-	if (fstat(out_fd, &sb) < 0) {
-		//treat error
-		exit(EXIT_FAILURE);
-	}
+	if (fstat(out_fd, &sb) < 0)
+		err_exit("fstat fd %d", out_fd);
 	out_dev = sb.st_dev;
 	out_ino = sb.st_ino;
 	out_isreg = S_ISREG(sb.st_mode);
@@ -72,14 +63,13 @@ int is_same_rfile(int in_fd, char *fname)
 
 	if (!fname)
 		fname = "<redirected>";
-	fprintf(stderr, "> ignoring input file (%s) "
-		"since it's the same as the output\n", fname);
-	if ((flags = fcntl(out_fd, F_GETFL)) < 0) {
-		// treat error
-	}
+	err("ignoring input file (%s) "
+	    "since it's the same as the output", fname);
+	if ((flags = fcntl(out_fd, F_GETFL)) < 0)
+		err_exit("fcntl fd %d", out_fd);
 	if (!(flags & O_APPEND))
-		fprintf(stderr, "> output file (%s) wasn't open in "
-			"append mode then probably truncated\n", fname);
+		err("output file (%s) wasn't open in "
+		    "append mode then probably truncated", fname);
 	return 1;
 }
 
@@ -89,11 +79,11 @@ int main(int argc, char *argv[])
 	int in_fd = STDIN_FILENO;
 	int ret = EXIT_SUCCESS;
 
+	app_set_name(argv[0]);
+
 	if (argc == 1) {
-		if (is_same_rfile(in_fd, NULL)) {
-			// treat error
-			exit(EXIT_FAILURE);
-		}
+		if (is_same_rfile(in_fd, NULL))
+			err_exit("input = output");
 		cat(in_fd);
 		return ret;
 	}
@@ -101,17 +91,14 @@ int main(int argc, char *argv[])
 	closefd(in_fd);
 	while (*(++file)) {
 		if ((in_fd = open(*file, O_RDONLY)) < 0) {
-			// treat error
-			fprintf(stderr, "> error opening file (%s)\n", *file);
+			err("opening file (%s)", *file);
 			ret = EXIT_FAILURE;
 			continue;
 		}
-		if (is_same_rfile(in_fd, *file)) {
-			// treat error
+		if (is_same_rfile(in_fd, *file))
 			ret = EXIT_FAILURE;
-		} else {
+		else
 			cat(in_fd);
-		}
 		closefd(in_fd);
 	}
 	return ret;
