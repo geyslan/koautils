55032dd1eab65e6b22e9e2ccfbae29c183d1d650
Geyslan G. Bem <geyslan@gmail.com>
2019-07-05 20:37:01 -0300

    cat: massive refactoring
    - is_same_rfile() now tests two informed file descriptors, returns 0 if they not point to the same regular file. Otherwise it returns O_CREAT or O_APPEND.
    - The new 'limit' variable in main() is filled with the index of the last non-literal arg option for further control.
    - Treat all cases inside 'do while' loop.
    
    Signed-off-by: Geyslan G. Bem <geyslan@gmail.com>

--- src/cat.c
+++ src/cat.c
@@ -30,9 +30,8 @@ void cat(int in_fd)
 		err_exit("reading from fd %d", in_fd);
 }
 
-int is_same_rfile(int in_fd, char *fname)
+int is_same_rfile(int in_fd, int out_fd)
 {
-	int out_fd = STDOUT_FILENO;
 	struct stat sb;
 
 	dev_t out_dev, in_dev;
@@ -57,64 +56,72 @@ int is_same_rfile(int in_fd, char *fname)
 	    out_ino == in_ino))
 		return 0;
 
-	if (!fname)
-		fname = "<redirected>";
-	err("ignoring input file (%s) "
-	    "since it's the same as the output", fname);
 	if ((flags = fcntl(out_fd, F_GETFL)) < 0)
 		err_exit("fcntl fd %d", out_fd);
-	if (!(flags & O_APPEND))
-		err("output file (%s) wasn't open in "
-		    "append mode then probably truncated", fname);
-	return 1;
+	return (flags & O_APPEND) ? O_APPEND : O_CREAT;
 }
 
 int main(int argc, char *argv[])
 {
-	char *input;
 	int opt;
-	int u_flag = 0;
+	int limit = argc;
 	int ret = EXIT_SUCCESS;
 
 	app_set_name(argv[0]);
+
+	/* get limit of non literal args */
+	for (int i = 0; i < argc; ++i) {
+		if (!strcmp(argv[i], "--")) {
+			limit = i;
+			break;
+		}
+	}
 
 	while ((opt = getopt(argc, argv, "u")) != -1) {
 		switch (opt) {
 		case 'u':
 			/* POSIX COMPLIANT */
 			/* read and write are always (u)nbuffered */
-			u_flag = 1;
 			break;
 		default:
 			err_exit("Usage: %s [-u] [files]", app_get_name());
 		}
 	}
-	if (u_flag)
-		++argc;
 
-	if (!argv[optind]) {
-		if (is_same_rfile(STDIN_FILENO, NULL))
-			err_exit_n(EXIT_FAILURE, "input = output");
-		cat(STDIN_FILENO);
-		return ret;
-	}
-	while ((input = argv[optind++])) {
-		if ((optind < argc) && (!strcmp(input, "-"))) {
-			cat(STDIN_FILENO);
-			continue;
-		}
-		int in_fd;
-		if ((in_fd = open(input, O_RDONLY)) < 0) {
+	do {
+		char *input;
+		int in_fd = -1;
+		int same;
+
+		input = argv[optind];
+
+		if ((!input) ||
+		    ((!strcmp(input, "-")) && (optind <= limit))) {
+			in_fd = STDIN_FILENO;
+		} else if ((in_fd = open(input, O_RDONLY)) < 0) {
 			err("opening file (%s)", input);
 			ret = EXIT_FAILURE;
 			continue;
 		}
-		if (is_same_rfile(in_fd, input))
-			ret = EXIT_FAILURE;
-		else
+		same = is_same_rfile(in_fd, STDOUT_FILENO);
+		if (!same) {
 			cat(in_fd);
+			if (in_fd == STDIN_FILENO)
+				continue;
+		} else {
+			ret = EXIT_FAILURE;
+			if (!input)
+				err_exit_n(ret, "input = output");
+			err("ignoring input file (%s) "
+			    "since it's the same as the output", input);
+			if (same == O_CREAT) {
+				err("output file wasn't open in "
+				    "append mode then probably truncated");
+			}
+		}
 		if (close(in_fd) < 0)
 			err_exit("closing fd %d", in_fd);
-	}
+	} while (++optind < argc);
+
 	return ret;
 }
